a39c8204115e344cfd30f8233a6fe38b511ad6db
Luciano <lorelloluciano68@gmail.com>
2022-11-23 14:42:08 -0300

    Funciona

--- probandoIA.py
+++ probandoIA.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 fahrenheit = np.array([32, 33.8, 50, 69, 57, 25, 0], dtype=float)
 celsius = np.array([0, 1, 10, 20.555, 13.888, -3.888, -17.777], dtype=float)
@@ -8,10 +9,14 @@ capa = tf.keras.layers.Dense(units=1, input_shape=[1])
 modelo = tf.keras.Sequential([capa])
 
 modelo.compile(
-    optimizer=tf.keras.optimizers.Adam(0.1),
+    optimizer=tf.keras.optimizers.Adam(0.07),
     loss='mean_squared_error'
 )
 
 print("Comenzando entrenamiento!!!")
-entrenamiento = modelo.fit(fahrenheit, celsius, epochs=500,verbose=False)
+entrenamiento = modelo.fit(fahrenheit, celsius, epochs=1000,verbose=False)
 print("Listo...")
+resultado = modelo.predict([70.0])
+print("\nTu resultado puede ser que sea " + str(resultado) + "?")
+
+
